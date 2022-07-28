46e14641453c147e18321586e657bf775e77b0cf
username <fruttys@gmail.com>
2022-07-28 19:59:09 +0300

    add node server

--- 1paskaita/index.js
+++ 1paskaita/index.js
@@ -10,22 +10,19 @@ console.log(casual.integer((from = 10), (to = 25)));
 // sukonstruoti user objektą iš casual random proerčių
 // turi turėti vardą, pavardę, lytį, adrasas (objektas su šalis, miestas, gatvė), el. paštas, slaptažodis, metai (0-99), kuri mėn gimė., mėgstamiausia spalva
 const user = casual.user;
-let color = casual.random_value({ a: "violet", b: "red", c: "black" });
-let birthmonth = casual.random_value({
-  a: "january",
-  b: "february",
-  c: "march",
-});
+const sex = ["male", "female", "other"];
 
 casual.define("user", function () {
   return {
     firstname: casual.first_name,
     lastname: casual.last_name,
+    sex: sex[casual.integer((from = 0), (to = 2))],
     email: casual.email,
     password: casual.password,
     address: casual.address,
-    color: color.random_value,
-    birthmonth: birthmonth.random_value,
+    integer: casual.integer((from = 0), (to = 99)),
+    month_name: casual.month_name,
+    color_name: casual.color_name,
   };
 });
 console.log(casual.user);
